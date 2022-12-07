47392c0cb817cd2ea2d298ee0adb87ddd1bee923
matty147 <88089429+matty147@users.noreply.github.com>
2022-12-07 18:31:07 +0100

    First Commit

---
+++ static arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace static_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = int.MaxValue, max = int.MinValue;
+            int[] array = {};
+            Console.WriteLine("Length of the array");
+            int lenght = int.Parse(Console.ReadLine());
+            array = new int[lenght];
+            for (int i = 0; i < lenght; i++)
+            {
+                Console.WriteLine("Number: " + (i + 1));
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("-------------------");
+            foreach (int number in array)
+            {
+                Console.WriteLine(number);
+                if (min > number)
+                {
+                    min = number;
+                }
+                if (max < number)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine("Smallest number: " + (min));
+            Console.WriteLine("Biggest number: " + (max));
+            Console.ReadKey();
+        }
+    }
+}
